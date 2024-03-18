544b139d848ff09ea36764eedcb9b55881d5aa0f
Arikaran-13 <arikaranofficial@gmail.com>
2024-03-18 21:07:36 +0530

    not completed todo task status number updated

--- src/app/task-status/task-status.component.ts
+++ src/app/task-status/task-status.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { filter } from "rxjs";
+import { Task } from "src/models/task";
+import { TaskService } from "src/service/task.service";
 
 @Component({
     selector: "task-status",
@@ -6,5 +9,21 @@ import { Component } from "@angular/core";
     styleUrls: ["./task-status.component.css"]
 })
 export class TaskStatusComponent{
+    
+    constructor(private taskService: TaskService){
 
+    }
+    completedTodo:number;
+    notCompltedTodo:number;
+
+    ngDoCheck(){
+        this.updateTodoTaskStatus();
+    }
+
+    public updateTodoTaskStatus(){
+            this.taskService.getTodoTasks()
+            .subscribe((data:Task[])=>{
+               this.notCompltedTodo= data.filter((task)=>task.status=== "Not completed").length
+            })
+    }
 }
